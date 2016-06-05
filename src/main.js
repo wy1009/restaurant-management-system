9bbdb87ce19949525d42c73b8c6c1d87cd541d17
wy1009 <wy941009@gmail.com>
2016-06-05 23:18:27 +0800

    test router

--- src/main.js
+++ src/main.js
@@ -12,16 +12,13 @@ Vue.use(VueResource)
 var router = new VueRouter()
 routerMap(router)
 router.beforeEach(function (transition) {
-    window.scrollTo(0, 0)
     var login = getLoginStatus(store.state)
     if (transition.to.name == 'login') {
         transition.next()
     } else {
         if (login) {
-            console.log(true)
             transition.next()
         } else {
-            console.log(false)
             transition.redirect({ name: 'login'})
         }
     }
--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,5 +1,3 @@
 export const loginSuccess = function ({dispatch, state}, userInfo) {
     dispatch('LOGINSUCCESS', userInfo)
-    this.$router.auth = true
-    console.log(this.$router)
 }

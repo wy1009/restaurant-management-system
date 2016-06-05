a830170bb014273a49df64e970126e867c249a7c
wy1009 <wy941009@gmail.com>
2016-06-05 20:16:59 +0800

    if not login, redirect

--- src/main.js
+++ src/main.js
@@ -12,14 +12,18 @@ Vue.use(VueResource)
 var router = new VueRouter()
 routerMap(router)
 router.beforeEach(function (transition) {
-    // window.scrollTo(0, 0)
-    // var login = getLoginStatus(store.state)
-    // if (login) {
-    //     console.log(true)
-        // transition.next()
-    // } else {
-    //     console.log(false)
-        transition.redirect('/login')
-    // }
+    window.scrollTo(0, 0)
+    var login = getLoginStatus(store.state)
+    if (transition.to.name == 'login') {
+        transition.next()
+    } else {
+        if (login) {
+            console.log(true)
+            transition.next()
+        } else {
+            console.log(false)
+            transition.redirect({ name: 'login'})
+        }
+    }
 })
 router.start(App, '#app')

d667efb668a21ca0e77e239128047ecd08d603fb
wy1009 <wy941009@gmail.com>
2016-06-06 05:12:38 +0800

    add chart

--- src/views/Overviews.vue
+++ src/views/Overviews.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import Chart from 'chart.js'
+
 export default {
     data () {
         return {
@@ -28,9 +30,36 @@ export default {
         }
     },
     ready () {
-        var ctx = document.getElementById("myChart").getContext("2d");
-        var myNewChart = new Chart(ctx).PolarArea(this.chartData);
-        console.log('a');
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+                datasets : [{
+                        fillColor : "rgba(220,220,220,0.5)",
+                        strokeColor : "rgba(220,220,220,1)",
+                        pointColor : "rgba(220,220,220,1)",
+                        pointStrokeColor : "#fff",
+                        data : [65,59,90,81,56,55,40]
+                    },{
+                        fillColor : "rgba(151,187,205,0.5)",
+                        strokeColor : "rgba(151,187,205,1)",
+                        pointColor : "rgba(151,187,205,1)",
+                        pointStrokeColor : "#fff",
+                        data : [28,48,40,19,96,27,100]
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
     }
 }
 </script>

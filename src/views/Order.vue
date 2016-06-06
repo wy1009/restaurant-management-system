54c4b6715cbd3d48f1e68fc6f53776c7e876549a
wy1009 <wy941009@gmail.com>
2016-06-06 11:17:11 +0800

    change order page without mixin

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,18 +1,86 @@
+<template>
+    <div class="order-wrapper has-side-nav">
+        <aside>
+            <ul>
+                <li v-for="orderStatus in orderStatusList" :class="orderStatus.name == nowOrderStatus.name ? 'active' : ''">
+                    <div @click="getorderList(orderStatus)">
+                        {{ orderStatus.name }}
+                    </div>
+                </li>
+            </ul>
+        </aside>
+        <article class="ui list-wrap">
+            <h3 class="ui title">
+                {{ nowOrderStatus.name }}
+                <span @click="toggleOrderDlg" v-if="orderStatus == 50">添加员工</span>
+            </h3>
+            <div class="list">
+                <ul>
+                    <li v-for="order in orderList">
+                        <h4 class="title fl">{{ order.name }}</h4>
+                        <div class="operation fr">
+                            <span @click="editorder(order)" v-if="orderStatus == 50">编辑</span>
+                        </div>
+                        <div class="phone fr">{{ order.phone }}</div>
+                    </li>
+                </ul>
+            </div>
+        </article>
+        <dlg-order-add-put v-show="dlgOrderShow" type="order" :info-obj="selectedOrderObj" :order-status-list="orderStatusList" @close-dlg="toggleOrderDlg" @submited="dlgOrderSubmited" transition="expand"></dlg-order-add-put>
+    </div>
+</template>
+
 <script>
-import DlgItemInfoAddPut from '../components/DlgOrderPut.vue'
-import ViewsMixin from './ViewsMixin.vue'
+import DlgOrderAddPut from '../components/DlgOrderAddPut.vue'
 
 export default {
-    mixins: [ViewsMixin],
     data () {
         return {
-            classType: 'order-status',
-            itemType: 'order',
-            filterName: 'status'
+            nowOrderStatus: {},
+            orderStatusList: [],
+            orderList: [],
+            selectedOrderObj: {},
+            dlgOrderShow: false
+        }
+    },
+    ready () {
+        this.getOrderList()
+    },
+    methods: {
+        getOrderList (orderStatus) {
+            if (orderStatus) {
+                this.nowOrderStatus = orderStatus
+            }
+            this.$http.get('/api/order/', this.nowOrderStatus).then(function (res) {
+                var data = res.data
+                if (data.success) {
+                    this.orderList = data.orderList
+                } else {
+                    console.log(data.reason)
+                }
+            })
+        },
+        toggleOrderDlg () {
+            this.dlgOrderShow = !this.dlgOrderShow
+            if (!this.dlgOrderShow) {
+                this.selectedOrderObj = {}
+            }
+        },
+        dlgOrderSubmited () {
+            this.getOrderList()
+            this.toggleOrderDlg()
+        },
+        editOrder (order) {
+            this.selectedOrderObj = order
+            this.toggleOrderDlg()
         }
     },
     components: {
-        DlgItemInfoAddPut
+        DlgOrderPut
+    },
+    vuex: {
+        getters: {
+        }
     }
 }
 </script>
